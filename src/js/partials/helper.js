f72021436d28ee5f8015faec7b6fd2352ff3a718
Alecsandr86 <alecsandrcherneckiy@gmail.com>
2016-04-14 22:27:48 +0300

    first

---
+++ src/js/partials/helper.js
@@ -0,0 +1,22 @@
+//Так приятней :)
+window.log = function(param){
+    console.log(param);
+};
+$(document).ready(function(){
+
+    //Chrome Smooth Scroll
+    try {
+        $.browserSelector();
+        if($("html").hasClass("chrome")) {
+            $.smoothScroll();
+        }
+    } catch(err) {
+
+    };
+
+
+    $('span').bind('click',function(){
+        alert('Все работает!!! :)')
+    })
+
+});
