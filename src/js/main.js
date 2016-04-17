ec164f080712967d927a220be21d2870496f40ae
Alecsandr86 <alecsandrcherneckiy@gmail.com>
2016-04-17 13:07:13 +0300

    anim-1

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,9 @@
  */
 //= ../../bower_components/jquery/dist/jquery.min.js
 //= ../../bower_components/bxslider-4/dist/jquery.bxslider.min.js
+//= ../../bower_components/gsap/src/minified/easing/EasePack.min.js
+//= ../../bower_components/gsap/src/minified/TimelineMax.min.js
+//= ../../bower_components/gsap/src/minified/TweenMax.min.js
 
 /*
     Custom
--- src/js/partials/helper.js
+++ src/js/partials/helper.js
@@ -11,15 +11,27 @@ $(document).ready(function(){
             $.smoothScroll();
         }
     } catch(err) {
-
     };
 
-
+    //  слайдер
     $('.slid-reviews').bxSlider({
         pager: false,
         adaptiveHeight: true,
         nextText:"<i class='slid-reviews-next'></i>",
         prevText:"<i class='slid-reviews-prev'></i>"
     });
+    //  \слайдер\
+
+    // TimelineLite instance
+    var tl = new TimelineLite();
+    
+    $("body").on("click",function () {
+        tl.to($('.lb-l'), 1 ,{width:"100%",opacity:'1'})
+            .to($('.lb-l'), 0 ,{opacity:'0'})
+              .to($('.box-wr-nav ,.wrap-descr-box'),1, {top:'0'})
+                .to($(".nav-box"),0,{overflow:"visible"})
+        
+    });
+    
 
 });
